e121e86183a0eab7344c9acadf3b41190fbea3c2
tlawowns7 <tlawowns78@naver.com>
2020-05-01 00:28:29 +0900

    재준작업
    로비 1차작업

---
+++ GD_ThreeMatch/Assets/8.GD/Ithilien/GirlSel.cs
@@ -0,0 +1,31 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlSel : MonoBehaviour
+{
+    public SkeletonAnimation skeletonAnimation;
+    public SkeletonDataAsset data_Alice;
+    public SkeletonDataAsset data_Beryl;
+
+    // Update is called once per frame
+    void Start()
+    {
+        if (skeletonAnimation.skeletonDataAsset != null)
+        {
+            if (skeletonAnimation.skeletonDataAsset == data_Alice)
+            {
+                Debug.Log("앨리스 출력");
+            }
+        }
+        if (skeletonAnimation.skeletonDataAsset != null)
+        {
+            if (skeletonAnimation.skeletonDataAsset == data_Beryl)
+            {
+                Debug.Log("배릴 출력");
+            }
+        }
+
+    }
+}
