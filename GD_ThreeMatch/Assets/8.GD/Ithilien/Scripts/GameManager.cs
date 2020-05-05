34094fd0ad7ea5acbbfe5247297542409fe8e28a
tlawowns7 <tlawowns78@naver.com>
2020-05-05 19:55:28 +0900

    재준작업

---
+++ GD_ThreeMatch/Assets/8.GD/Ithilien/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Button gameStartBtn;
+    
+
+    [Header("Top UI")]
+    public Text text_Timer;
+    public Text text_Count;
+
+    public int maxStamina;
+    public float filledTime;
+
+    
+    private int currentStamina = 3; // 테스트 추후 변경
+    private float copyTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentStamina = maxStamina;
+        copyTime = filledTime;
+
+        text_Count.text = currentStamina + " / " + maxStamina;
+
+        if (gameStartBtn != null)
+        {
+            if (currentStamina > 0)
+            {
+                gameStartBtn.onClick.AddListener(() =>
+                {
+                    currentStamina--;
+                    SceneManager.LoadScene("InGame");
+                });
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(currentStamina < maxStamina)
+        {
+            filledTime -= Time.deltaTime;
+            text_Timer.text = GetMinute(filledTime) + ":" + GetSecond(filledTime);
+        }
+
+        if(filledTime <= 0)
+        {
+            if (currentStamina < maxStamina)
+            {
+                currentStamina++;
+                filledTime = copyTime;
+                text_Count.text = currentStamina + " / " + maxStamina;
+            }
+        }
+    }
+
+    public static int GetMinute(float _time)
+    {
+        return (int)((_time / 60) % 60);
+    }
+
+    public static int GetSecond(float _time)
+    {
+        return (int)(_time % 60);
+    }
+}
---
+++ GD_ThreeMatch/Assets/8.GD/Ithilien/Scripts/GirlSel.cs
@@ -0,0 +1,43 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CharacterState
+{
+    NULL = 0,
+    Alice,
+    Beryl
+}
+
+
+public class GirlSel : MonoBehaviour
+{
+    public SkeletonAnimation skeletonAnimation = null;
+    public SkeletonDataAsset data_Alice;
+    public SkeletonDataAsset data_Beryl;
+
+    public Button aliceBtn;
+    public Button BerylBtn;
+    public CharacterState characterState = CharacterState.NULL;
+
+    // Update is called once per frame
+    void Start()
+    {
+        aliceBtn.onClick.AddListener(() =>
+        {
+            characterState = CharacterState.Alice;
+            skeletonAnimation.skeletonDataAsset = data_Alice;
+        });
+
+        BerylBtn.onClick.AddListener(() =>
+        {
+            characterState = CharacterState.Beryl;
+            skeletonAnimation.skeletonDataAsset = data_Beryl;
+        });
+    }
+
+    
+}
+
